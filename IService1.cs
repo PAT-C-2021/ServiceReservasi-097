3dcd0b1aabe21b45a2e0a73ab8f4436100ebab54
ffadelm <faysalfadelm0107@gmail.com>
2021-12-09 11:13:20 +0700

    Praktikum 9 - Edit dan Delete

--- IService1.cs
+++ IService1.cs
@@ -14,7 +14,7 @@ namespace ServiceReservasi
         [OperationContract]
         string pemesanan(string IDPemesanan, string NamaCustomer, string NoTelpon, int JumlahPemesanan, string IDLokasi);
         [OperationContract]
-        string editPemesanan(string IDPemesanan, string NamaCustomer);
+        string editPemesanan(string IDPemesanan, string NamaCustomer, string No_telpon);
         [OperationContract]
         string deletePemesanan(string IDPemesanan);
 
@@ -52,7 +52,7 @@ namespace ServiceReservasi
     public class Pemesanan
     {
         [DataMember]
-        public int IDPemesanan { get; set; }
+        public string IDPemesanan { get; set; }
         [DataMember]
         public string NamaCustomer { get; set; }
         [DataMember]
@@ -60,7 +60,7 @@ namespace ServiceReservasi
         [DataMember]
         public int JumlahPemesanan { get; set; }
         [DataMember]
-        public int IDLokasi { get; set; }
+        public string Lokasi { get; set; }
     }
 
 }
--- Service1.cs
+++ Service1.cs
@@ -43,6 +43,36 @@ namespace ServiceReservasi
             return LokasiFull;
         }
 
+        public List<Pemesanan> Pemesanan()
+        {
+            List<Pemesanan> pemesanans = new List<Pemesanan>();
+            try
+            {
+                string sql = "SELECT ID_reservasi, Nama_customer, No_telpon, Jumlah_pemesanan, Nama_Lokasi FROM dbo.Pemesanan p join dbo. p.ID_lokasi = l.ID_lokasi";
+                conn = new SqlConnection(koneksi);
+                comm = new SqlCommand(sql, conn);
+                conn.Open();
+                SqlDataReader reader = comm.ExecuteReader();
+                while (reader.Read())
+                {
+                    Pemesanan data = new Pemesanan();
+                    data.IDPemesanan = reader.GetString(0);
+                    data.NamaCustomer = reader.GetString(1);
+                    data.NoTelepon = reader.GetString(2);
+                    data.JumlahPemesanan = reader.GetInt32(3);
+                    data.Lokasi = reader.GetString(4);
+                    pemesanans.Add(data);
+
+                }
+                conn.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return pemesanans;
+        }
+
         public string pemesanan(string IDPemesanan,  string NamaCustomer, string NoTelpon, int JumlahPemesanan, string IDLokasi)
         {
             string n = "gagal";
@@ -54,7 +84,16 @@ namespace ServiceReservasi
                 conn.Open();
                 comm.ExecuteNonQuery();
                 conn.Close();
+
+                string sql2 = "UPDATE dbo.Lokasi set Kuota = Kuota - "+JumlahPemesanan+" WHERE ID_lokasi = '"+IDLokasi+"' ";
+                conn = new SqlConnection(koneksi);
+                comm = new SqlCommand(sql2, conn);
+                conn.Open();
+                comm.ExecuteNonQuery();
+                conn.Close();
+
                 n = "Berhasil";
+
             }catch(Exception ex)
             {
                 Console.WriteLine(ex);
@@ -62,19 +101,47 @@ namespace ServiceReservasi
             return n;
         }
 
-        public string editPemesanan(string IDPemesanan, string NamaCustomer)
+        public string editPemesanan(string IDPemesanan, string NamaCustomer, string No_telpon)
         {
-            throw new NotImplementedException();
+            string n = "gagal";
+            try
+            {
+                string sql = "UPDATE dbo.Pemesanan SET Nama_customer = '" + NamaCustomer + "', No_telpon = '" + No_telpon + "' WHERE ID_reservasi = '" + IDPemesanan + "' ";
+                conn = new SqlConnection(koneksi);
+                comm = new SqlCommand(sql, conn);
+                conn.Open();
+                comm.ExecuteNonQuery();
+                conn.Close();
+
+                n = "Berhasil";
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return n;
         }
 
         public string deletePemesanan(string IDPemesanan)
         {
-            throw new NotImplementedException();
+            string n = "gagal";
+            try
+            {
+                string sql = "DELETE FROM dbo.Pemesanan WHERE ID_reservasi = '" + IDPemesanan + "' ";
+                conn = new SqlConnection(koneksi);
+                comm = new SqlCommand(sql, conn);
+                conn.Open();
+                comm.ExecuteNonQuery();
+                conn.Close();
+
+                n = "Berhasil";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return n;
         }
 
-        public List<Pemesanan> Pemesanan()
-        {
-            throw new NotImplementedException();
-        }
     }
 }
