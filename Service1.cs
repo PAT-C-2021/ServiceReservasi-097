07a707d6d3963dc9880689052ed588dfc77b5c61
ffadelm <faysalfadelm0107@gmail.com>
2021-12-25 22:58:59 +0700

    praktikum 11 😍

--- Service1.cs
+++ Service1.cs
@@ -165,5 +165,126 @@ namespace ServiceReservasi
         {
             throw new NotImplementedException();
         }
+
+        public string Login(string username, string password)
+        {
+            string kategori = "";
+
+            string sql = "select Kategori from Login where Username= '" + username + "' and Password='" + password + "'";
+            conn = new SqlConnection(koneksi);
+            comm = new SqlCommand(sql, conn);
+            conn.Open();
+            SqlDataReader reader = comm.ExecuteReader();
+            while (reader.Read())
+            {
+                kategori = reader.GetString(0);
+            }
+            return kategori;
+        }
+
+        public string Register(string username, string password, string kategori)
+        {
+            string n = "gagal";
+            try
+            {
+                string sql = "INSERT INTO Login VALUES('" + username + "','" + password + "','" + kategori + "')";
+                conn = new SqlConnection(koneksi);
+                comm = new SqlCommand(sql, conn);
+                conn.Open();
+                comm.ExecuteNonQuery();
+                conn.Close();
+
+                n = "Berhasil";
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return n;
+        }
+
+        public string UpdateRegister(string username, string password, string kategori, int id)
+        {
+            string n = "gagal";
+            try
+            {
+                string sql = "UPDATE Login SET Username = '" + username + "', Password = '" + password + "', kategori = '"+kategori+"' Where ID_login = "+id+"";
+                conn = new SqlConnection(koneksi);
+                comm = new SqlCommand(sql, conn);
+                conn.Open();
+                comm.ExecuteNonQuery();
+                conn.Close();
+
+                n = "Berhasil";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return n;
+        }
+
+        public string DeleteRegister(string username)
+        {
+            string n = "gagal";
+            try
+            {
+                int id = 0;
+                string sql = "SELECT ID_login FROM dbo.Login WHERE Username = '" + username + "' ";
+                conn = new SqlConnection(koneksi);
+                comm = new SqlCommand(sql, conn);
+                conn.Open();
+                SqlDataReader reader = comm.ExecuteReader();
+                while (reader.Read())
+                {
+                    id = reader.GetInt32(0);
+                }
+                conn.Close();
+
+                string sql2 = "DELETE FROM dbo.Login WHERE ID_login=" + id + "";
+                conn = new SqlConnection(koneksi);
+                comm = new SqlCommand(sql2, conn);
+                conn.Open();
+                comm.ExecuteNonQuery();
+                conn.Close();
+
+                n = "Berhasil";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return n;
+        }
+
+        public List<DataRegister> DataRegist()
+        {
+            List<DataRegister> list = new List<DataRegister>();
+            try
+            {
+                string sql = "SELECT ID_login, Username, Password, Kategori from Login";
+                conn = new SqlConnection(koneksi);
+                comm = new SqlCommand(sql, conn);
+                conn.Open();
+                SqlDataReader reader = comm.ExecuteReader();
+                while (reader.Read())
+                {
+                    DataRegister data = new DataRegister();
+                    data.id = reader.GetInt32(0);
+                    data.username = reader.GetString(1);
+                    data.password = reader.GetString(2);
+                    data.kategori = reader.GetString(3);
+                    list.Add(data);
+
+                }
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return list;
+        }
     }
 }
